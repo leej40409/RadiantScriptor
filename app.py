927d0d0c36c8eb580e8051adbcef7aeb0d68c7dd
Jeniffer Lee <114110918+leej40409@users.noreply.github.com>
2023-11-25 03:56:44 -0600

    Update app.py
    Signed-off-by: Jeniffer Lee <114110918+leej40409@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,30 +1,101 @@
-from transformers import pipeline
+
 import streamlit as st
+import requests
 
-# Set the page configuration
-st.set_page_config(page_title="RadiantScriptor")
+# Function to call the Hugging Face model
+def query_huggingface_model(prompt):
+    API_TOKEN = "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"  # Replace with your Hugging Face API token
+    API_URL = "https://api-inference.huggingface.co/models/MariamAde/Mistral_finetuned_Base2"  # Replace with your model's API URL
 
-# Initialize the pipeline for text generation
-# Note: The pipeline will automatically handle the device allocation (CPU/GPU)
-pipe = pipeline("text-generation", model="MariamAde/Mistral_finetuned_Base2")
+    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    response = requests.post(API_URL, headers=headers, json={"inputs": prompt})
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": response.text}
 
-# Function to generate the report
-def generate_report(labels):
-    # Generate output using the pipeline
-    output = pipe(labels, max_new_tokens=100, do_sample=True)
-    # The output is a list of dictionaries, each containing the generated text
-    sentences = output[0]['generated_text']
-    return sentences
+# Streamlit interface
+def main():
+    st.title("My Fine-tuned Model Demo")
 
-# Streamlit interface for user interaction
-st.title("Radiology Report Generator")
-labels = st.text_input("Enter Finding Labels:")
+    # User input
+    user_input = st.text_area("Enter your text here", "")
 
-# Button to generate the report
-if st.button("Generate Report"):
-    report = generate_report(labels)
-    st.text_area("Generated Report:", value=report, height=300)
+    # Button to make the prediction
+    if st.button("Predict"):
+        with st.spinner("Predicting..."):
+            response = query_huggingface_model(user_input)
+            if "error" in response:
+                st.error(response["error"])
+            else:
+                st.success("Prediction Success")
+                st.write(response)  # Modify this based on how your model's response is structured
 
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############################################################################
+# from transformers import pipeline
+# import streamlit as st
+
+# # Set the page configuration
+# st.set_page_config(page_title="RadiantScriptor")
+
+# # Initialize the pipeline for text generation
+# # Note: The pipeline will automatically handle the device allocation (CPU/GPU)
+# pipe = pipeline("text-generation", model="MariamAde/Mistral_finetuned_Base2")
+
+# # Function to generate the report
+# def generate_report(labels):
+#     # Generate output using the pipeline
+#     output = pipe(labels, max_new_tokens=100, do_sample=True)
+#     # The output is a list of dictionaries, each containing the generated text
+#     sentences = output[0]['generated_text']
+#     return sentences
+
+# # Streamlit interface for user interaction
+# st.title("Radiology Report Generator")
+# labels = st.text_input("Enter Finding Labels:")
+
+# # Button to generate the report
+# if st.button("Generate Report"):
+#     report = generate_report(labels)
+#     st.text_area("Generated Report:", value=report, height=300)
+
+
+
+
+################################################################################
 # from transformers import AutoModelForCausalLM, AutoTokenizer
 # import streamlit as st
 # import torch
