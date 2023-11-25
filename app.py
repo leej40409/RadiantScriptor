b33243d86a9654f36ac1cc2e56ede53532125abd
Jeniffer Lee <114110918+leej40409@users.noreply.github.com>
2023-11-24 23:45:06 -0600

    Update app.py
    Signed-off-by: Jeniffer Lee <114110918+leej40409@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import streamlit as st
-device = "cuda" # the device to load the model onto
+import torch
+
+# Check for GPU availability, default to CPU if not available
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Set the page configuration
 st.set_page_config(page_title="RadiantScriptor")
 
 # Caching the model loading function to improve performance
-@st.cache(allow_output_mutation=True)
+@st.cache_data(allow_output_mutation=True)
 def get_model():
     # Load tokenizer and model
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
     model = AutoModelForCausalLM.from_pretrained("gpt2")
     return tokenizer, model
 
-
-# Load model and tokenizer with a spinner to indicate loading process
+# Load model and tokenizer with a spinner
 with st.spinner('Model is being loaded..'):
       tokenizer, model = get_model()
 
-# Function to generate the radiology report
+# Function to generate the report
 def generate_report(labels):
     # Tokenize the input labels
     inputs = tokenizer(labels, return_tensors="pt").to(device)
@@ -26,19 +28,14 @@ def generate_report(labels):
     # Generate output using the model
     output = model.generate(**inputs, max_new_tokens=100, do_sample=True)
     # Decode the output sentences
-   #sentences = tokenizer.decode(output[0], skip_special_tokens=True)
     sentences = tokenizer.batch_decode(output[0])
     return sentences
 
 # Streamlit interface for user interaction
 st.title("Radiology Report Generator")
-
-# User input for finding labels
 labels = st.text_input("Enter Finding Labels:")
 
 # Button to generate the report
 if st.button("Generate Report"):
-    # Generate the radiology report
     report = generate_report(labels)
-    # Display the report
     st.text_area("Generated Report:", value=report, height=300)
