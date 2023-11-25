6daf9d7e1262e7d4dd0f961723568459f90b68f6
Jeniffer Lee <114110918+leej40409@users.noreply.github.com>
2023-11-25 01:36:44 -0600

    Update app.py
    Signed-off-by: Jeniffer Lee <114110918+leej40409@users.noreply.github.com>

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ st.set_page_config(page_title="RadiantScriptor")
 @st.experimental_singleton
 def get_model():
     # Load tokenizer and model
-    tokenizer = AutoTokenizer.from_pretrained("MariamAde/Mistral_finetuned_v2") #MariamAde/Mistral_finetuned_Base2
-    model = AutoModelForCausalLM.from_pretrained("MariamAde/Mistral_finetuned_v2").to(device)
+    tokenizer = AutoTokenizer.from_pretrained("models/MariamAde/Mistral_finetuned_v2") #MariamAde/Mistral_finetuned_Base2
+    model = AutoModelForCausalLM.from_pretrained("models/MariamAde/Mistral_finetuned_v2").to(device)
     return tokenizer, model
 
 # Load model and tokenizer with a spinner
